3035c05df268ed45b58d67fea985dca5909550ff
pumkinni <102509636+pumkinni@users.noreply.github.com>
2023-01-02 18:03:44 +0900

    Add files via upload

---
+++ Practice1.java
@@ -0,0 +1,89 @@
+// Practice1
+// 주어진 이진 탐색 트리에서 N 번째로 작은 수 구하기
+
+// 입력 트리: 3, 1, 4, null, 2
+// N: 1
+// 결과: 1
+
+// 입력 트리: 5, 3, 6, 2, 4, null, null, 1
+// N: 3
+// 결과: 3
+
+import java.util.ArrayList;
+
+class Node {
+    int key;
+    Node left;
+    Node right;
+
+    Node(int key, Node left, Node right) {
+        this.key = key;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class BinarySearchTree {
+    Node head;
+
+    BinarySearchTree() {}
+    BinarySearchTree(int key) {
+        this.head = new Node(key, null, null);
+    }
+
+    public void addNode(int key) {
+        this.head = this.addNode(this.head, key);
+    }
+
+    public Node addNode(Node cur, int key) {
+        if (cur == null) {
+            return new Node(key, null, null);
+        }
+
+        if (key < cur.key) {
+            cur.left = addNode(cur.left, key);
+        } else {
+            cur.right = addNode(cur.right, key);
+        }
+
+        return cur;
+    }
+    public ArrayList inOrder(ArrayList list, Node node){
+        Node cur = node;
+        if (cur.left != null){
+            inOrder(list, cur.left);
+        }
+        list.add(cur.key);
+
+        if(cur.right != null){
+            inOrder(list, cur.right);
+        }
+        return list;
+    }
+}
+
+public class Practice1 {
+    public static void solution(Integer[] data, int n) {
+        ArrayList list = new ArrayList<>();
+        BinarySearchTree bst = new BinarySearchTree();
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] != null){
+                bst.addNode(data[i]);
+            }
+        }
+
+        list = bst.inOrder(list, bst.head);
+        System.out.println(list.get(n-1));
+    }
+
+    public static void main(String[] args) {
+        // Test code
+        Integer[] data = {3, 1, 4, null, 2};
+        int n = 1;
+        solution(data, n);
+
+        data = new Integer[]{5, 3, 6, 2, 4, null, null, 1};
+        n = 3;
+        solution(data, n);
+    }
+}
